9cfc0c7e371bfbed276e6269fccfcb3feef2c2c4
Brianna Iliev <brianna.iliev@sumup.com>
2020-02-24 16:17:17 +0200

    plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
+const HTMLWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
     module: {
         rules: [
             {
+                enforce: 'pre',
                 test: '/\.js$/',
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                use: ["babel-loader", "eslint-loader"],
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+
+    ]
 }
