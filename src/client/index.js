d0ba04d31ce11aa0fa2a2b7398e968273d55b99c
Brianna Iliev <brianna.iliev@sumup.com>
2020-02-25 18:14:41 +0200

    additions to the exercise

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@ import { myFunction } from './js/nameChecker'
 import { thisFunction } from './js/formHandler'
 // import './styles/resets.scss'
 
+
+export {
+    myFunction,
+    thisFunction
+}
+
 console.log(checkForName);
 
 alert("I EXIST")
